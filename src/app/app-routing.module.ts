87b9ba2c2259eee94cd2140aac34b97961d0cbbb
pawelwojkiewicz <p.wojkiewicz@gmail.com>
2021-02-26 22:07:25 +0100

    structure of login and register modules, installed bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./modules/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'registration',
+    loadChildren: () =>
+      import('./modules/registration/registration.module').then(m => m.RegistrationModule)
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
