defd99914ec7d1645a9278ae8f49cabf07ceafaa
pawelwojkiewicz <p.wojkiewicz@gmail.com>
2021-04-14 15:39:47 +0200

    added login reactive form with primary validation

--- src/app/modules/login/pages/login/login.component.ts
+++ src/app/modules/login/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,55 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initLoginForm();
   }
 
+  initLoginForm(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl(
+        null,
+        [
+          Validators.email,
+          Validators.required
+        ]
+      ),
+      password: new FormControl(
+        null,
+        [
+          Validators.required,
+        ],
+      ),
+    }
+    );
+  }
+
+  get logForm(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+
+  validateAllFormFields(formGroup: FormGroup): void {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+        control.markAsDirty({ onlySelf: true });
+        control.updateValueAndValidity();
+      }
+    });
+  }
+
+  onLogin(): void {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
+      console.log(email, password);
+    } else {
+      this.validateAllFormFields(this.loginForm);
+    }
+  }
 }
