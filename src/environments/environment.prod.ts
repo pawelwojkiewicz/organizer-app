52a99a4ad053c3d1a6e341e9f37769e91b4f1e74
pawelwojkiewicz <p.wojkiewicz@gmail.com>
2021-07-04 16:09:41 +0200

    push docs folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  registerApiUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBl7PiYrxj5G_O4PhMaGjGoC3n8-U1VvWk',
+  loginApiUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBl7PiYrxj5G_O4PhMaGjGoC3n8-U1VvWk',
+  dataStorageUrl: 'https://organizer-app-29a22-default-rtdb.firebaseio.com',
 };
